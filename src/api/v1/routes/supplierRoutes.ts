8fbfe629bc54b3cb81a97291d7eb0e77e508e021
rrc_f2024_arishi <arishi@academic.rrc.ca>
2026-04-04 23:11:56 -0500

    feat: Created router for supplier endpoints.

---
+++ src/api/v1/routes/supplierRoutes.ts
@@ -0,0 +1,12 @@
+import express, { Router } from "express";
+import * as supplierController from '../controllers/supplierController';
+
+const router: Router = express.Router();
+
+router.get('/', supplierController.getAll);
+router.get('/:id', supplierController.getById);
+router.post('/', supplierController.create);
+router.put('/:id', supplierController.update);
+router.delete('/:id', supplierController.deleteSupplier);
+
+export default router;
