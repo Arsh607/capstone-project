5ebed4a9edbe12ae7fa0d135d596dae01fe393a1
rrc_f2024_arishi <arishi@academic.rrc.ca>
2026-04-06 14:05:41 -0500

    feat: Created validation schema for product endpoints.

---
+++ src/api/v1/validation/productValidation.ts
@@ -0,0 +1,19 @@
+import Joi from "joi";
+
+export const createProductValidation = Joi.object({
+    name: Joi.string().min(1).required(),
+    description: Joi.string().min(1).required(),
+    price: Joi.number().min(0).required(),
+    quantity: Joi.number().min(0).required(),
+    category: Joi.string().valid("Electronics", "Office Supplies", "Furniture", "Food", "Home Supplies").required(),
+    supplierId: Joi.string().pattern(/^supp_\d+$/).required()
+}).unknown(false);
+
+export const updateProductValidation = Joi.object({
+    name: Joi.string().min(1),
+    description: Joi.string().min(1),
+    price: Joi.number().min(0),
+    quantity: Joi.number().min(0),
+    category: Joi.string().valid("Electronics", "Office Supplies", "Furniture", "Food", "Home Supplies"),
+    supplierId: Joi.string().pattern(/^supp_\d+$/)
+}).unknown(false);
