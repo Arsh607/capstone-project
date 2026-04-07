e7c828ce46e80ef6f3eaef622bd5c65cb592d345
rrc_f2024_arishi <arishi@academic.rrc.ca>
2026-04-07 16:00:54 -0500

    feat: Integrated error handler in product services.

--- src/api/v1/services/productServices.ts
+++ src/api/v1/services/productServices.ts
@@ -1,29 +1,49 @@
 import * as productRepository from '../repositories/productRepository';
+import * as supplierRepository from '../repositories/supplierRepository';
 import { CreateProductInput, Product, UpdateProductInput } from '../models/productModel';
+import { AppError } from '../utils/AppError';
+import { HTTP_STATUS } from '../../../constants/httpsConstants';
 
 export const getAll = (): Promise<Product[]> => {
-    return productRepository.getAllProducts();
+  return productRepository.getAllProducts();
 };
 
 export const getById = (id: string): Promise<Product | null> => {
-    return productRepository.getProductById(id);
+  return productRepository.getProductById(id);
 };
 
 export const createProduct = async (data: CreateProductInput): Promise<Product> => {
-    const nextId = await productRepository.getNextProductIdFromDB();
+  const supplier = await supplierRepository.getSupplierByIdFromDb(data.supplierId);
 
-    const newProduct: Product = {
-        id: nextId,
-        ...data
-    };
+  if (!supplier) {
+    throw new AppError('Supplier not found', HTTP_STATUS.NOT_FOUND);
+  }
 
-    return productRepository.createNewProduct(newProduct);
+  const nextId = await productRepository.getNextProductIdFromDB();
+
+  const newProduct: Product = {
+    id: nextId,
+    ...data
+  };
+
+  return productRepository.createNewProduct(newProduct);
 };
 
-export const updateProduct = (id: string, data: UpdateProductInput): Promise<Product | null> => {
-    return productRepository.updateProductFromDB(id, data);
+export const updateProduct = async (
+  id: string,
+  data: UpdateProductInput
+): Promise<Product | null> => {
+  if (data.supplierId) {
+    const supplier = await supplierRepository.getSupplierByIdFromDb(data.supplierId);
+
+    if (!supplier) {
+      throw new AppError('Supplier not found', HTTP_STATUS.NOT_FOUND);
+    }
+  }
+
+  return productRepository.updateProductFromDB(id, data);
 };
 
 export const deleteProduct = (id: string): Promise<Product | null> => {
-    return productRepository.deleteProductFromDB(id);
+  return productRepository.deleteProductFromDB(id);
 };
