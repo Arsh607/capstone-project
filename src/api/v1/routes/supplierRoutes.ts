5fa7a19c6167d015f9ddf7e44699349195b05ee4
rrc_f2024_arishi <arishi@academic.rrc.ca>
2026-04-07 15:20:32 -0500

    chore: Added joi to the supplier routes.

--- src/api/v1/routes/supplierRoutes.ts
+++ src/api/v1/routes/supplierRoutes.ts
@@ -1,12 +1,16 @@
 import express, { Router } from "express";
 import * as supplierController from '../controllers/supplierController';
-
+import { createSupplierValidation, updateSupplierValidation, supplierIdValidation } from "../validation/supplierValidation";
+import { validateBody, validateParams } from "../middleware/validation";
 const router: Router = express.Router();
 
 router.get('/', supplierController.getAll);
-router.get('/:id', supplierController.getById);
-router.post('/', supplierController.create);
-router.put('/:id', supplierController.update);
-router.delete('/:id', supplierController.deleteSupplier );
+router.get('/:id', validateParams(supplierIdValidation), supplierController.getById);
+router.post('/', validateBody(createSupplierValidation), supplierController.create);
+router.put('/:id', 
+    validateParams(supplierIdValidation),
+    validateBody(updateSupplierValidation),
+    supplierController.update);
+router.delete('/:id', validateParams(supplierIdValidation), supplierController.deleteSupplier );
 
 export default router;
