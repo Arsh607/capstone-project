8022c63142ead4bf4ff23ba556fc93b09a731174
rrc_f2024_arishi <arishi@academic.rrc.ca>
2026-04-07 15:23:29 -0500

    chore: Added joi to the transaction routes.

--- src/api/v1/routes/transactionRoutes.ts
+++ src/api/v1/routes/transactionRoutes.ts
@@ -1,12 +1,17 @@
 import express, { Router } from "express";
 import * as inventoryController from '../controllers/transactionController';
+import { createTransactionValidation, updateTransactionValidation, transactionIdValidation } from "../validation/transactionValidation";
+import { validateParams, validateBody } from "../middleware/validation";
 
 const router: Router = express.Router();
 
 router.get('/', inventoryController.getAll);
-router.get('/:id', inventoryController.getById);
-router.post('/', inventoryController.create);
-router.put('/:id', inventoryController.update);
-router.delete('/:id', inventoryController.deleteTransaction);
+router.get('/:id', validateParams(transactionIdValidation), inventoryController.getById);
+router.post('/', validateBody(createTransactionValidation),inventoryController.create);
+router.put('/:id',
+    validateParams(transactionIdValidation), 
+    validateBody(updateTransactionValidation), 
+    inventoryController.update);
+router.delete('/:id', validateParams(transactionIdValidation), inventoryController.deleteTransaction);
 
 export default router;
